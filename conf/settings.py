776b3e75a8f665a2a2a7d8011238411e9bf266ff
islam <nudinovislam4@gmail.com>
2021-10-23 14:41:26 +0600

    create settings_local.py

--- conf/settings.py
+++ conf/settings.py
@@ -13,6 +13,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from settings_local import key
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0vq07v8m*nf%dalkfi5uj^yk$hzso8zp9m%&bzk3n3j7774lf@'
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
