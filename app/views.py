cd8d4f377391aaf4267674d2837903d11f4ca620
islam <nudinovislam4@gmail.com>
2021-10-25 18:59:06 +0600

    create in views xml_to_json and udate index.html & urls.py

--- app/views.py
+++ app/views.py
@@ -1,3 +1,15 @@
+from django.core.exceptions import ValidationError
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def xml_to_json(request):
+    if request.method == 'POST':
+        xml = request.FILES['xml']
+        print(xml.content_type)
+        print(xml.size)
+        if xml.content_type != 'text/xml':
+            raise ValidationError("File is not XML.")
+
+    return render(request, 'index.html')
--- conf/urls.py
+++ conf/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from app import views
+
 urlpatterns = [
+    path('', views.xml_to_json, name= "xml_to_json"),
     path('admin/', admin.site.urls),
 ]
